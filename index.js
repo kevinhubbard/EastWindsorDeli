6d2a45c09793a1d9199d2d96887ff5b610b0e906
Kevin Hubbard <dgkallday98@gmail.com>
2017-09-11 17:52:56 -0400

    updated views

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var path = require('path');
+var fs = require('fs');
 var port = process.env.PORT || 8080;
 
 var app = express();
@@ -12,6 +13,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
 	res.render('index');
+});
+
+app.get('/menu', function(req, res){
+	var tempFile = '/public/menu/ewd.pdf';
+	fs.readFile(__dirname + tempFile, function(err, data){
+		res.contentType('application/pdf');
+		res.send(data);
+	});
 });
 
 app.listen(port, function(){
